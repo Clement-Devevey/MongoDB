e58761a28df96aaccb57071112cdeed423dfe5c1
clément-devevey <poubelledekikoo@gmail.com>
2021-10-09 11:20:51 +0200

    Add files via upload

--- match.js
+++ match.js
@@ -2,14 +2,14 @@ db.match.insert({
     domicile:'Team Vitality',
     exterieur:'Natus Vincere',
     competition:'Blast Premier',
-    score_domicile:'2',
-    score_ext:'1',
-    joueurs_domicile: {
-          pseudo: ['Zywoo','Apex','Shox','Misutaa','Kyojin'],
-          note: ['10','8','9','7','8']
+    score_domicile:2,
+    score_ext:1,
+    joueurs_domicile: {
+          pseudo: db.equipe.findOne( { "nom": "Team Vitality" } ).effectif,
+          note: [10,8,9,7,8]
         },
-    joueur_ext:{
-          pseudo: ['S1mple','electronic','Perfecto','Boombl4','b1t'],
-          note: ['9','7','7','6','7']
+    joueurs_ext:{
+          pseudo: db.equipe.findOne( { "nom": "Natus Vincere" } ).effectif,
+          note: [9,7,7,6,7]
         }
 });
